d21016b6bc951054cb60a1b1d1d3794ef12564ad
Tony Hua <tonyhua18@gmail.com>
2024-03-09 15:56:03 -0800

    removed api view decorators

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,16 +1,13 @@
-from django.contrib.auth import authenticate, login, logout
 from rest_framework import status, views
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import UserSerializer
-from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate, login, logout, get_user_model
 
 # get our custom user model from db
 User = get_user_model()
 
 # User signup methods
 class UserCreate(views.APIView):
-    @api_view(['POST'])
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
@@ -20,7 +17,6 @@ class UserCreate(views.APIView):
 
 # user login methods
 class LoginView(views.APIView):
-    @api_view(['POST'])
     def post(self, request):
         email = request.data.get('email')
         password = request.data.get('password')
@@ -32,13 +28,11 @@ class LoginView(views.APIView):
 
 # user logout methods
 class LogoutView(views.APIView):
-    @api_view(['GET'])
     def get(self, request):
         logout(request)
         return Response({'message': 'User Logged Out'})
 
 # sample hello world view
 class HelloWorld(views.APIView):
-    @api_view(['GET'])
     def get(self, request):
-        return Response({'message': 'Hello World!'})
+        return Response({'message': 'Hello World GET request is working!'})
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-3%$)q3+3m5ca8+z2a3-m3)-jx=j$-q_kohz#!akjl8-(-)2h8f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
