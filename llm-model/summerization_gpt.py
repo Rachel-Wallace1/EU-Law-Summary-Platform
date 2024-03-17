3dfff0c2cba3b021e235ecd1e6aff5963ece01a5
mjaymehdi <mjaymehdi@gmail.com>
2024-03-17 19:23:19 -0400

    LAW summerization using GPT-3.5 api

---
+++ llm-model/summerization_gpt.py
@@ -0,0 +1,51 @@
+# !pip install langchain
+# !pip install pypdf
+# !pip install openai
+
+import urllib
+from pathlib import Path as p
+
+from langchain import PromptTemplate
+from langchain.chains.summarize import load_summarize_chain
+from langchain.document_loaders import PyPDFLoader
+from langchain.llms import OpenAI
+
+import os
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+llm = OpenAI(model_name = 'gpt-3.5-turbo-16k')
+
+data_folder = p.cwd() / "data"
+p(data_folder).mkdir(parents=True, exist_ok=True)
+
+pdf_url = "https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:32014R1144"
+pdf_file = str(p(data_folder, pdf_url.split("/")[-1]))
+
+urllib.request.urlretrieve(pdf_url, pdf_file)
+
+pdf_loader = PyPDFLoader(pdf_file)
+pages = pdf_loader.load_and_split()
+print(pages[3].page_content)
+
+prompt_template = """Write a concise summary of the following text delimited by triple backquotes.
+              Return your response in bullet points which covers the key points of the text.
+              ```{text}```
+              BULLET POINT SUMMARY:
+  """
+
+prompt = PromptTemplate(template=prompt_template, input_variables=["text"])
+
+stuff_chain = load_summarize_chain(llm, chain_type="stuff", prompt=prompt)
+
+total = 0
+for i, page in enumerate(pages):
+  total += len(page.page_content.split())
+  print(f'Page {i:2d} - Length {len(page.page_content.split())}')
+print(total)
+
+print(pages[0].page_content)
+
+content = pages[:4]
+
+print(stuff_chain.run(content))
