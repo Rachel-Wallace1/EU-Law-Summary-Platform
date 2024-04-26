a6a0405488232f3b4bcd88871253e4eef2b3e6f6
Rachel Wallace <64049629+Rachel-Wallace1@users.noreply.github.com>
2024-04-25 18:32:26 -0700

    integrate public citizen view with backend (#135)

--- frontend/src/components/SummariesComponent.js
+++ frontend/src/components/SummariesComponent.js
@@ -5,8 +5,6 @@ import Table from "./SummariesTableComponents/TableComponent";
 function SummariesComponent({category}) {
     const [summaryList, setSummaryList] = useState();
 
-    console.log(category);
-
     useEffect(() => {
         const fetchSummaryList = async () => {
             let allData = [];
@@ -15,15 +13,12 @@ function SummariesComponent({category}) {
 
             try {
                 while (hasMoreData) {
-                    const response = await fetch(`${process.env.NODE_ENV === 'development' ? process.env.REACT_APP_API_URL_LOCAL : process.env.REACT_APP_API_URL_DNS}/api/fetchAll/${pageIndex}`);
-
-                    // TODO add once backend category and page as part of query params
-                    // let query = ""
-                    // query += `page=${pageIndex}`
-                    // if (category !== "") {
-                    //     query += `&category=${category}`
-                    // }
-                    // const response = await fetch(`${process.env.NODE_ENV === 'development' ? process.env.REACT_APP_API_URL_LOCAL : process.env.REACT_APP_API_URL_DNS}/api/fetchAll?${query}`);
+                    let query = ""
+                    query += `page=${pageIndex}`
+                    if (category && category !== "") {
+                        query += `&category=${category}`
+                    }
+                    const response = await fetch(`${process.env.NODE_ENV === 'development' ? process.env.REACT_APP_API_URL_LOCAL : process.env.REACT_APP_API_URL_DNS}/api/fetchAll?${query}`);
 
                     if (!response.ok) {
                         throw new Error(`Status: ${response.status}`);
